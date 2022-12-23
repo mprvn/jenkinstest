0d52fff0884eae15bbae3a8f773141f666236970
praveen moolan <mln.prvn@gmail.com>
2022-12-24 00:36:26 +0530

    break app

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,6 @@
 public class HelloWorld{
 	public static  void main(String[] args){
-		
-		System.out.println(hello world jenkins);
-		System.out.println("*******hello world jenkins **********");
+		wqeqweqwe
+		System.out.println(hello world jenkins)
 	}
 }
